5e6414b279bfec975ba756ad3483a928fca9ec52
RebalancedShowdown <66094079+RebalancedShowdown@users.noreply.github.com>
2020-06-01 14:15:53 -0600

    full build

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (a1610dd7)";
+/* version */ Config.version = "0.11.2 (b54def90)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
